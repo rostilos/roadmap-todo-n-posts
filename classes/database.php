13db19443a6c6c08e3639013d46d250f35e232a1
Ростислав Сулейманов <rostilosfl@gmail.com>
2023-06-14 14:28:07 +0300

    added backend functionality for login/register, jwt tokens

---
+++ classes/database.php
@@ -0,0 +1,124 @@
+<?php
+
+class Database
+{
+    private $server_name = 'localhost';
+    private $database_username = 'root';
+    private $database_password = '';
+    private $database_name = 'roadmap';
+    private $connection = null;
+
+    public function register($user)
+    {
+        $this->connection = new mysqli(
+            $this->server_name,
+            $this->database_username,
+            $this->database_password,
+            $this->database_name
+        );
+        $this->connection->set_charset('utf8');
+        $sql = $this->connection->prepare(
+            'INSERT INTO users (`firstname`, `lastname`, `password`, `email`, `birth_date`) VALUES (?,?,?,?,?)'
+        );
+        $sql->bind_param(
+            'sssss',
+            $user['firstname'],
+            $user['lastname'],
+            $user['password'],
+            $user['email'],
+            $user['birth_date']
+        );
+        if ($sql->execute()) {
+            $id = $this->connection->insert_id;
+            $sql->close();
+            $this->connection->close();
+            return $id;
+        }
+        $sql->close();
+        $this->connection->close();
+        return false;
+    }
+
+    public function loginUser($email, $password)
+    {
+        $this->connection = new mysqli(
+            $this->server_name,
+            $this->database_username,
+            $this->database_password,
+            $this->database_name
+        );
+        $this->connection->set_charset('utf8');
+        $sql = $this->connection->prepare(
+            'SELECT * FROM `users` WHERE email=? AND password=?'
+        );
+        $sql->bind_param('ss', $email, $password);
+        $sql->execute();
+        $result = $sql->get_result();
+        if ($result->num_rows > 0) {
+            $user = $result->fetch_assoc();
+            $sql->close();
+            $this->connection->close();
+            return $user;
+        }
+        $sql->close();
+        $this->connection->close();
+        return false;
+    }
+
+    public function getUserByEmail($email)
+    {
+        $this->connection = new mysqli(
+            $this->server_name,
+            $this->database_username,
+            $this->database_password,
+            $this->database_name
+        );
+        $this->connection->set_charset('utf8');
+        $sql = $this->connection->prepare(
+            'SELECT DISTINCT * FROM `user` WHERE username=? OR email=?'
+        );
+        $sql->bind_param('ss', $username, $username);
+        $sql->execute();
+        $result = $sql->get_result();
+        if ($result->num_rows > 0) {
+            $user = $result->fetch_assoc();
+            $sql->close();
+            $this->connection->close();
+            return $user;
+        }
+        $sql->close();
+        $this->connection->close();
+        return false;
+    }
+
+    // public function updateUser($user)
+    // {
+    //     $this->connection = new mysqli(
+    //         $this->server_name,
+    //         $this->database_username,
+    //         $this->database_password,
+    //         $this->database_name
+    //     );
+    //     $this->connection->set_charset('utf8');
+    //     $sql = $this->connection->prepare(
+    //         'UPDATE `user` SET `name` = ?,`lastname`=?,`username`=?,`password`=?,`email`=? WHERE id=?'
+    //     );
+    //     $sql->bind_param(
+    //         'sssssi',
+    //         $user['name'],
+    //         $user['lastname'],
+    //         $user['username'],
+    //         $user['password'],
+    //         $user['email'],
+    //         $user['id']
+    //     );
+    //     if ($sql->execute()) {
+    //         $sql->close();
+    //         $this->connection->close();
+    //         return true;
+    //     }
+    //     $sql->close();
+    //     $this->connection->close();
+    //     return false;
+    // }
+}
