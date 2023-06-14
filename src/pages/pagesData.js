2514a0329c524e77d23d2cfe767115c3350add45
Ростислав Сулейманов <rostilosfl@gmail.com>
2023-06-14 14:29:32 +0300

    routes configuration sample for future improvements

---
+++ src/pages/pagesData.js
@@ -0,0 +1,20 @@
+import Login from "../pages/Login/Login";
+// import Notes from "../pages/Notes/Notes";
+// import PostList from "../pages/Posts/PostList";
+// import PostView from "../pages/Posts/PostView";
+// import UserAccount from "../pages/UserAccount/UserAccount";
+
+const pagesData = [
+  {
+    path: "/login",
+    element: <Login />,
+    title: "login",
+  },
+  // {
+  //   path: "notes",
+  //   element: <About />,
+  //   title: "notes"
+  // }
+];
+
+export default pagesData;
