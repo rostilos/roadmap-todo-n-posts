69ee69136ed5ad6184bf793d4ed3716b3e1d50d0
Ростислав Сулейманов <rostilosfl@gmail.com>
2023-07-04 00:19:06 +0300

    dashboard fixes

--- src/pages/UserAccount/UserAccount.jsx
+++ src/pages/UserAccount/UserAccount.jsx
@@ -4,11 +4,15 @@ import useAppContext from "../../hook/useAppContext";
 const UserAccount = function () {
   const { userData } = useAppContext();
   return (
-    <div className="user-account _section">
-      <p>{userData?.firstname}</p>
-      <p>{userData?.lastname}</p>
-      <p>{userData?.birth_date}</p>
-      <p>{userData?.email}</p>
+    <div>
+      <div className="user-account _section">
+      <h1 className="page__title">Account Information</h1>
+
+        <p>{userData?.firstname}</p>
+        <p>{userData?.lastname}</p>
+        <p>{userData?.birth_date}</p>
+        <p>{userData?.email}</p>
+      </div>
     </div>
   );
 };
