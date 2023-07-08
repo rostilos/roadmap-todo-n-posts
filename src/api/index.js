15aa1afb28ca0a6bb389961c5f45d27aba1ea724
Ростислав Сулейманов <rostilosfl@gmail.com>
2023-07-08 22:03:48 +0300

    delete/update note functionality added (2)

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,13 @@
 import ajaxLogin from "./user/login";
 import register from "./user/register";
-import fetchNotes from "./notes/fetch/fetchNotes";
-import createNote from "./notes/create/createNote";
+import fetchNotes from "./notes/fetch/";
+import createNote from "./notes/create";
+import editNote from "./notes/edit";
+import deleteNote from "./notes/delete";
 
 export const ajaxLoginRequest = ajaxLogin;
 export const registerRequest = register;
 export const fetchNotesRequest = fetchNotes;
 export const createNoteRequest = createNote;
+export const editNoteRequest = editNote;
+export const deleteNoteRequest = deleteNote;
